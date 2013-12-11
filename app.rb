dd2418a5a3e1b565082c3df558e8141200634d91
Nikki Thean <nikki.thean@gmail.com>
2013-12-11 12:03:08 -0500

    Add youtube embedding\!

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require_relative './lib/artist'
 require_relative './lib/database'
 require_relative './lib/genre'
 require_relative './lib/song'
+require 'open-uri'
 
 require 'bundler'
 Bundler.require
@@ -33,10 +34,28 @@ module Playlister
 
     get '/:choice' do |choice|
       @choice = search(choice)
-      erb symbolize(@choice.class)
+      if @choice
+        @url = get_url("https://gdata.youtube.com/feeds/api/videos?q=#{@choice.artist.name.gsub(/[\s\-]/, "%20")}+#{@choice.name.gsub(" ", "%20")}+music+video").match(/\=(.*)&/)[1] if @choice.is_a?(Song)
+        erb symbolize(@choice.class)
+      else
+        erb :not_found
+      end
+    end
+
+    not_found do
+      erb :not_found
     end
 
     helpers do
+      def get_url(url)
+        html = Nokogiri::HTML(open(url.gsub(" ", "%20")))
+        html.xpath('//link[contains(@href, "https://www.youtube.com/watch")]').map {|link| link['href']}.first
+      end
+
+      def pluralize(list, word)
+        list.size > 1 ? word + "s" : word
+      end
+
       def search(choice)
         @artists.detect {|element| element.name == choice} || @songs.detect {|element| element.name == choice} || @genres.detect {|element| element.name == choice}
       end
@@ -44,38 +63,10 @@ module Playlister
       def symbolize(word)
         word.to_s.downcase.to_sym
       end
-    end
 
-    helpers do
-
-        def simple_partial(template)
-          erb template
-        end
-    
-        def intermediate_partial(template, locals=nil)
-          locals = locals.is_a?(Hash) ? locals : {template.to_sym => locals}
-          template = :"_#{template}"
-          erb template, {}, locals        
-        end
-   
-        def adv_partial(template,locals=nil)
-          if template.is_a?(String) || template.is_a?(Symbol)
-            template = :"_#{template}"
-          else
-            locals=template
-            template = template.is_a?(Array) ? :"_#{template.first.class.to_s.downcase}" : :"_#{template.class.to_s.downcase}"
-          end
-          if locals.is_a?(Hash)
-            erb template, {}, locals      
-          elsif locals
-            locals=[locals] unless locals.respond_to?(:inject)
-            locals.inject([]) do |output,element|
-              output << erb(template,{},{template.to_s.delete("_").to_sym => element})
-            end.join("\n")
-          else 
-            erb template
-          end
-        end
+      def simple_partial(template)
+        erb template
       end
+    end
   end
 end
